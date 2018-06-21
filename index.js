66d6db52b83d213574a21b466dfd8e5835db1269
George Higbie <gehigbie@gmail.com>
2018-06-21 16:17:21 +0000

    Initial commit

---
+++ index.js
@@ -0,0 +1,21 @@
+function createStore () {
+    let state
+    let listeners = [];
+    
+    const getState = () => state;
+    const subscribe =  (listener) => {
+        listeners.push(listener);
+        return () => {
+            listeners = listeners.filter((l) => l !== listener);
+        };
+    };
+    return {
+        getState, 
+        subscribe
+    };
+}
+
+const store = createStore();
+store.subscribe(() => {
+    console.log(`The new state is: ${store.getState()}`);
+})
